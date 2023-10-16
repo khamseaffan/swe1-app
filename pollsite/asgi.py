7c6ed46bf87c1330a9759fa450324cfbc3df0992
Affan Khamse <khamseaffan@10-18-171-46.dynapool.wireless.nyu.edu>
2023-10-16 16:38:53 -0400

    change'

---
+++ pollsite/asgi.py
@@ -0,0 +1,21 @@
+"""
+ASGI config for pollsite project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+from django.urls import include, path
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pollsite.settings')
+
+application = get_asgi_application()
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
