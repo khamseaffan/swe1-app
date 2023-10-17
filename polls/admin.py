5ad56a5a85acde8a38cd1fe316c2384a7089ca82
Affan Khamse <khamseaffan@Affans-Air.lan>
2023-10-17 16:58:51 -0400

    error resolved

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Question, Choice
+from .models import Question
 
 admin.site.register(Question)
-admin.site.register(Choice)
