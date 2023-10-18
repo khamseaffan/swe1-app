2e2fb45a4d0d1ef6c8997015a740216f74491ba2
Affan Khamse <66311597+khamseaffan@users.noreply.github.com>
2023-10-17 20:37:18 -0400

    Update asgi.py

--- pollsite/asgi.py
+++ pollsite/asgi.py
@@ -10,12 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from django.urls import include, path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pollsite.settings')
 
 application = get_asgi_application()
-urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
-]
