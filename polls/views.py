54cb82d59c1b4e934e2c63c24bcee50816497e75
khamseaffan <khamseaffan@gmail.com>
2023-10-17 22:57:44 -0400

    files formatted

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404, HttpResponseRedirect
-from django.template import loader
+from django.http import HttpResponseRedirect
+
 from django.views import generic
 from django.urls import reverse
 from .models import Question, Choice
